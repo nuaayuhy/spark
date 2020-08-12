551996c018f4965140f62da08da61164b9f87db4
nuaayuhy <1106658062@qq.com>
2020-08-12 22:29:02 +0800

    filter算子

---
+++ src/main/scala/com/nuaa/cs/spark/core/rdd/Spark12_RDD_filter.scala
@@ -0,0 +1,26 @@
+package com.nuaa.cs.spark.core.rdd
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object Spark12_RDD_filter {
+
+
+  def main(args: Array[String]): Unit = {
+
+    // TODO Spark - RDD -算子（方法）
+
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sparkContext = new SparkContext(sparkConf)
+
+    // filter
+    // 生成数据，按照指定的规则进行过滤
+    val listRDD:RDD[List[Int]] = sparkContext.makeRDD(List(1,2,3,4))
+
+    val filterRDD: RDD[List[Int]] = listRDD.filter(x=>x%2==0)
+
+    filterRDD.collect().foreach(println)
+
+    sparkContext.stop()
+  }
+}
